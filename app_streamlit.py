4fe1d7b184b85182187f81d20a3895c65b4f84aa
Chewye <werfa13q@gmail.com>
2023-08-30 20:10:37 +0500

    feat: end EDA

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,15 +3,46 @@ import streamlit as st
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+from scipy import stats
+
 
 plt.style.use("dark_background")
+
+feat_dict = {
+    'AGE': 'возраст клиента',
+    'GENDER': 'пол клиента (1 — мужчина, 0 — женщина)',
+    'EDUCATION': 'образование',
+    'MARITAL_STATUS': 'семейное положение',
+    'CHILD_TOTAL':'количество детей клиента',
+    'DEPENDANTS': 'количество иждивенцев клиента',
+    'SOCSTATUS_WORK_FL': 'социальный статус клиента относительно работы (1 — работает, 0 — не работает)',
+    'SOCSTATUS_PENS_FL': 'социальный статус клиента относительно пенсии (1 — пенсионер, 0 — не пенсионер)',
+    'REG_ADDRESS_PROVINCE': 'область регистрации клиента',
+    'FACT_ADDRESS_PROVINCE': 'область фактического пребывания клиента',
+    'POSTAL_ADDRESS_PROVINCE': 'почтовый адрес области',
+    'FL_PRESENCE_FL': 'наличие в собственности квартиры (1 — есть, 0 — нет)',
+    'OWN_AUTO': 'количество автомобилей в собственности',
+    'AGREEMENT_RK': 'уникальный идентификатор объекта в выборке',
+    'TARGET': 'целевая переменная: отклик на маркетинговую кампанию (1 — отклик был зарегистрирован, 0 — отклика не было)',
+    'GEN_INDUSTRY': 'отрасль работы клиента',
+    'GEN_TITLE': 'должность',
+    'JOB_DIR': 'направление деятельности внутри компании',
+    'WORK_TIME': 'время работы на текущем месте (в месяцах)',
+    'FAMILY_INCOME': 'семейный доход (несколько категорий)',
+    'PERSONAL_INCOME': 'личный доход клиента (в рублях)',
+    'CREDIT': 'сумма последнего кредита клиента (в рублях)',
+    'TERM': 'срок кредита',
+    'FST_PAYMENT': 'первоначальный взнос (в рублях)',
+    'ID_LOAN': 'идентификатор кредита',
+    'CLOSED_FL': 'текущий статус кредита (1 — закрыт, 0 — не закрыт)'
+}
 
 df = pd.read_csv('df.csv')
 
 st.header('Cource HSE "Linear models. EDA"')
 st.subheader('')
 
-st.write('Посмотрим на баланс целевого признака')
+st.write('Баланс целевого признака')
 fig = plt.figure()
 ax = sns.countplot(df.TARGET)
 ax.set (xlabel='Наличие отклика',
@@ -47,21 +78,23 @@ cat_feat = ['OWN_AUTO',
             'SOCSTATUS_WORK_FL']
 
 add_selectbox = st.selectbox(
-    "Choose type of feature",
+    "Выберите тип признака (непрерывный, категориальный)",
     ("Numerical", "Categorical")
 )
 st.text(f'You choose {add_selectbox} type.')
 
 if add_selectbox == 'Numerical':
     feature_choose = st.selectbox(
-        'Now choose feature',
+        'Выберите признак',
         numerical
     )
 else:
     feature_choose = st.selectbox(
-        'Now choose feature',
+        'Выберите признак',
         cat_feat
     )
+
+st.write(feat_dict[feature_choose])
 
 if add_selectbox == 'Categorical':
     st.write(f'Feature {feature_choose} has category {df[feature_choose].unique()}')
@@ -70,29 +103,46 @@ if add_selectbox == 'Categorical':
     ax = sns.countplot(df[feature_choose])
     ax.set (xlabel=feature_choose,
     ylabel='Count',
-    title=f'boxplot {feature_choose}')
+    title=f'countplot')
     st.pyplot(fig)
 
-    st.write(f'count in each category:')
+    st.write(f'Количество в каждой категории:')
     st.write(pd.DataFrame([[df[df[feature_choose]==cat].shape[0] for cat in df[feature_choose].unique()]], columns=df[feature_choose].unique()))
     
 
 else:
-    st.write(f'Feature {feature_choose} has range {df[feature_choose].min()} - {df[feature_choose].max()}')
+    st.write(f'Признак {feature_choose} имеет диапозон {df[feature_choose].min()} - {df[feature_choose].max()}')
 
     fig = plt.figure()
-    ax = sns.histplot(df[feature_choose])
+    ax = sns.histplot(df[feature_choose], kde=True)
     ax.set (xlabel=feature_choose,
     ylabel='Count',
-    title=f'histplot {feature_choose}')
+    title=f'histplot')
     st.pyplot(fig)
 
     st.write(df[[feature_choose]].describe().T)
 
+    fig = plt.figure()
+    ax = sns.boxplot(df[feature_choose])
+    ax.set (xlabel=feature_choose,
+    title=f'boxplot')
+    st.pyplot(fig)
+
+    st.write(f'Предварительные выбросы:')
+    if df[stats.zscore(df[feature_choose])< -3].shape[0] > 0:
+        st.write(f'до {df[stats.zscore(df[feature_choose])< -3][feature_choose].max()}')
+    if df[stats.zscore(df[feature_choose]) > 3].shape[0] > 0:
+        st.write(f'от {df[stats.zscore(df[feature_choose]) > 3][feature_choose].min()}')
+
+
+options = st.multiselect(
+    'Выберите признаки для генерации тепловой карты',
+    numerical + cat_feat + ['TARGET'],
+    ['TARGET', 'OWN_AUTO', 'PERSONAL_INCOME', 'GENDER', 'FAMILY_INCOME', 'CHILD_TOTAL'])
+
+
 
 fig = plt.figure()
-ax = sns.heatmap(df[numerical])
-ax.set (xlabel=f'-',
-ylabel='-',
-title=f'heatmap')
+ax = sns.heatmap(df[options].corr(), annot=True)
+ax.set (title=f'heatmap')
 st.pyplot(fig)
